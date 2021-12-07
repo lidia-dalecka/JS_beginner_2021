6e5bae608b519e35abc390b6c12ba3b310674cd8
Lidia Dalecka <lidia.dalecka@op.pl>
2021-12-07 18:33:12 +0100

    Changed title in index.html; Create variables and condition if/else in test.js

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-alert("Hello, World!");
+var youLikeMeat=true;
+var myNum=10;
+
+if (youLikeMeat){
+
+    document.write("You like meat");
+
+} else{
+
+    document.write("You hate meat"); 
+
+}
+
