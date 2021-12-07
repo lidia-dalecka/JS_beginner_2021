12d2e54718589db7f85d6ed5057c44d0e777adfa
Lidia Dalecka <lidia.dalecka@op.pl>
2021-12-07 18:48:32 +0100

    Delete previous content and added else if statement in test.js

--- test.js
+++ test.js
@@ -1,13 +1,17 @@
-var youLikeMeat=true;
-var myNum=10;
+var myAge=29;
 
-if (youLikeMeat){
+if (myAge>30){
 
-    document.write("You like meat");
+    document.write("You are over 30!");
 
-} else{
+} else if (myAge>20){
 
-    document.write("You hate meat"); 
+    document.write("You are over 20!"); 
 
+} else if (myAge>10){
+
+    document.write("You are over 10!"); 
+} else {
+
+    document.write("You are not over 10!"); 
 }
-
