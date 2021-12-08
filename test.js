c5cfef292d0b94061617749a5e2b50b58df3dea8
Lidia Dalecka <lidia.dalecka@op.pl>
2021-12-08 23:19:29 +0100

    Changed content in test.js; created Constructor Function

--- test.js
+++ test.js
@@ -1,14 +1,20 @@
-var myCar2 = {
-    maxSpeed: 70,
-    driver: "David",
-    drive: function (speed, time) { 
-        console.log(speed*time); 
-    },
-    logDriver: function(){
-        console.log("driver name is " + this.driver);
-    }
-};
-myCar2.logDriver();
-console.log(myCar2.maxSpeed);
-myCar2.drive(50, 3);
 
+
+var Car = function (maxSpeed, driver) {
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function (speed, time) {
+        console.log(speed * time);
+    };
+    this.logDriver = function () {
+        console.log("driver name is " + this.driver);
+    };
+}
+
+var myCar = new Car(70, "Lidia");
+var myCar2 = new Car(40, "Mia");
+var myCar3 = new Car(10, "John");
+var myCar4 = new Car(90, "Gretha");
+
+myCar.drive(30, 5);
+myCar3.logDriver();
