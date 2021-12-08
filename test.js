8f582b5d8518a02bc411a71f2d40c1c0f8e6268e
Lidia Dalecka <lidia.dalecka@op.pl>
2021-12-08 20:39:51 +0100

    Delete content test.js; Defined few variables of string type; sliced and splitted the strings

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
-var string1="Abc";
-var string2="Bcd";
+var str = "hello, world";
+var str2 = str.slice(2, 9);
 
-console.log(string1<string2);
+console.log(str2);
+
+var str3 = str.slice(2);
+console.log(str3);
+
+var tags = "meat, ham, salami, pork, beef, chicken";
+var tagsArray = tags.split(",");
+
+console.log(tagsArray);
